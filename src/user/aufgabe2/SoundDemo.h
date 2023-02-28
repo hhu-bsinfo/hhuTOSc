145f8bf75c78bc4cbddb3fda8f71b2f108c311d6
schoettner <michael.schoettner@hhu.de>
2023-02-28 21:40:43 +0100

    Aufgabe 2

---
+++ src/user/aufgabe2/SoundDemo.h
@@ -0,0 +1,16 @@
+/*****************************************************************************
+ *                                                                           *
+ *                         S O U N D D E M O                                 *
+ *                                                                           *
+ *---------------------------------------------------------------------------*
+ * Beschreibung:    Test für den PC-Lautsprecher-Treiber.                    *
+ *                                                                           *
+ * Autor:           Michael Schoettner, HHU, 26.10.2018                      *
+ *****************************************************************************/
+
+#ifndef __SoundDemo_include__
+#define __SondDemo_include__
+
+void sound_demo();  
+
+#endif
