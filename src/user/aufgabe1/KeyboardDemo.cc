993be51fd12ed275c5773edfd098bff5f38e0f4f
schoettner <michael.schoettner@hhu.de>
2023-02-28 21:08:38 +0100

    Aufgabe 1

---
+++ src/user/aufgabe1/KeyboardDemo.cc
@@ -0,0 +1,18 @@
+/*****************************************************************************
+ *                                                                           *
+ *                        K E Y B O A R D D E M O                            *
+ *                                                                           *
+ *---------------------------------------------------------------------------*
+ * Beschreibung:    Testausgaben für den CGA-Treiber.                        *
+ *                                                                           *
+ * Autor:           Michael Schoettner, HHU, 26.10.2018                      *
+ *****************************************************************************/
+
+#include "kernel/Globals.h"
+
+
+void keyboard_demo() {
+
+    /* Hier muess Code eingefuegt werden */
+
+}
---
+++ src/user/aufgabe1/KeyboardDemo.h
@@ -0,0 +1,16 @@
+/*****************************************************************************
+ *                                                                           *
+ *                        K E Y B O A R D D E M O                            *
+ *                                                                           *
+ *---------------------------------------------------------------------------*
+ * Beschreibung:    Testausgaben für den CGA-Treiber.                        *
+ *                                                                           *
+ * Autor:           Michael Schoettner, HHU, 26.10.2018                      *
+ *****************************************************************************/
+
+#ifndef __KeyboardDemo_include__
+#define __KeyboardDemo_include__
+
+void keyboard_demo();  
+
+#endif
