c1dd42835b804bee0ace0930497103eb27755273
schoettner <michael.schoettner@hhu.de>
2023-04-14 16:06:16 +0200

    Dateien hinzugefuegt

---
+++ src/user/aufgabe2/HeapDemo.cc
@@ -0,0 +1,29 @@
+/*****************************************************************************
+ *                                                                           *
+ *                            H E A P D E M O                                *
+ *                                                                           *
+ *---------------------------------------------------------------------------*
+ * Beschreibung:    Demonstration der dynamischen Speicherverwaltung.        *
+ *                                                                           *
+ * Autor:           Michael Schoettner, HHU, 27.12.2016                      *
+ *****************************************************************************/
+
+#include "kernel/Globals.h"
+#include "kernel/Allocator.h"
+#include "user/HeapDemo.h"
+#include "user/MyObj.h"
+
+
+// Hilfsfunktion: Auf Return-Taste warten
+void waitForReturn() {
+
+    /* hier muss Code eingefuegt werden */
+
+}
+
+
+void heap_demo() {
+
+   /* hier muss Code eingefuegt werden */
+
+}
---
+++ src/user/aufgabe2/SoundDemo.cc
@@ -0,0 +1,17 @@
+/*****************************************************************************
+ *                                                                           *
+ *                         S O U N D D E M O                                 *
+ *                                                                           *
+ *---------------------------------------------------------------------------*
+ * Beschreibung:    Test für den PC-Lautsprecher-Treiber.                    *
+ *                                                                           *
+ * Autor:           Michael Schoettner, HHU, 26.10.2018                      *
+ *****************************************************************************/
+
+#include "kernel/Globals.h"
+
+void sound_demo() {
+   
+   /* Hier muess Code eingefuegt werden */
+
+}
