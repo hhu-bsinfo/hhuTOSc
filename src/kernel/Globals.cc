15865dd64bf3d4fc689c831181ae7ad193f91506
schoettner <michael.schoettner@hhu.de>
2023-04-24 19:23:27 +0200

    Removed wrong includes

--- src/kernel/Globals.cc
+++ src/kernel/Globals.cc
@@ -10,18 +10,5 @@
 
 #include "kernel/Globals.h"
 
-
-CPU                   cpu;        // CPU-spezifische Funktionen
-PCSPK                 pcspk;      // PC-Lautsprecher
-CGA_Stream            kout;       // Ausgabe-Strom fuer Kernel
-Keyboard              kb;         // Tastatur
-IntDispatcher         intdis;     // Unterbrechungsverteilung
-PIC                   pic;        // Interrupt-Controller
-unsigned int          total_mem;  // RAM total
-//BumpAllocator         allocator;       
-LinkedListAllocator   allocator;       
-Scheduler             scheduler;  // Scheduler
-BIOS                  bios;       // Schnittstelle zum 16-Bit BIOS
-VESA_Stream           vout;       // Grafik-Ausgabe-Strom 
 unsigned long         systime=0;  // wird all 10ms hochgezaehlt
 PIT                   pit(10000); // Zeitgeber (10ms)
